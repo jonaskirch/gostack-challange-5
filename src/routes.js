b35105b95c4a3d7cd98f187caed157d366cc6898
jonas <jonas@office.br>
2020-02-07 15:23:02 -0300

    - new features

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/repository" component={Repository} />
+        <Route path="/repository/:repository" component={Repository} />
         <Route path="/" component={Main} />
       </Switch>
     </BrowserRouter>
--- src/styles/global.js
+++ src/styles/global.js
@@ -13,7 +13,7 @@ export default createGlobalStyle`
   }
 
   body {
-    background: #ccc;
+    background: #7159c1;
     -webkit-font-smoothing: antialiased !important;
   }
 
